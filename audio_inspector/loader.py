60591fb98d21597dc6fdf39724158df0b8ad071a
Juan Pablo Mounier <juanpablomounier@gmail.com>
2026-02-12 00:45:58 -0300

    Fix loader docstring to match actual behavior

--- audio_inspector/loader.py
+++ audio_inspector/loader.py
@@ -1,14 +1,10 @@
 """This script loads a valid audio file.
 The audio file's path is saved in the 'audio' variable.
-It's validated.
 The audio is loaded as mono and it's amplitud is normalized (-1 to +1).
 The function returns both audio signal and sample rate.
 """
 import librosa
 def load_audio(audio_path):
-    #Here the audio file is loaded and returns signal and sample rate
+    #Load audio file as mono waveform and sampling rate
     audio_signal, audio_sample_rate = librosa.load(audio_path, mono=True)
-
-
-
     return audio_signal, audio_sample_rate
