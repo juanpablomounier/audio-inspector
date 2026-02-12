39c542f2f9e70b1e2bf2bfbd00f226ac7c13cb3a
Juan Pablo Mounier <juanpablomounier@gmail.com>
2026-02-12 00:35:29 -0300

    Explicitly load audio in mono and document normalization

--- audio_inspector/loader.py
+++ audio_inspector/loader.py
@@ -1,12 +1,13 @@
 """This script loads a valid audio file.
 The audio file's path is saved in the 'audio' variable.
 It's validated.
+The audio is loaded as mono and it's amplitud is normalized (-1 to +1).
 The function returns both audio signal and sample rate.
 """
 import librosa
 def load_audio(audio_path):
     #Here the audio file is loaded and returns signal and sample rate
-    audio_signal, audio_sample_rate = librosa.load(audio_path)
+    audio_signal, audio_sample_rate = librosa.load(audio_path, mono=True)
 
 
 
