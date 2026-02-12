fcfe4ead71bdcf28a83e5683e089b35e25122232
Juan Pablo Mounier <juanpablomounier@gmail.com>
2026-02-12 00:28:25 -0300

    Implement audio loading using librosa

--- audio_inspector/loader.py
+++ audio_inspector/loader.py
@@ -6,7 +6,7 @@ The function returns both audio signal and sample rate.
 import librosa
 def load_audio(audio_path):
     #Here the audio file is loaded and returns signal and sample rate
-
+    audio_signal, audio_sample_rate = librosa.load(audio_path)
 
 
 
