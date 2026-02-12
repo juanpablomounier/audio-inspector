3dd5a323a186c7cf00d4b74bafc687b81268b405
Juan Pablo Mounier <juanpablomounier@gmail.com>
2026-02-12 00:23:40 -0300

    Refine loader interface and prepare audio loading step

--- audio_inspector/loader.py
+++ audio_inspector/loader.py
@@ -1,1 +1,13 @@
-#This script loads a valid audio file.
+"""This script loads a valid audio file.
+The audio file's path is saved in the 'audio' variable.
+It's validated.
+The function returns both audio signal and sample rate.
+"""
+import librosa
+def load_audio(audio_path):
+    #Here the audio file is loaded and returns signal and sample rate
+
+
+
+
+    return audio_signal, audio_sample_rate
