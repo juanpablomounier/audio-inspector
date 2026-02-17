678de7b800fc67f5c8bb3abb857e41d32612f25a
Juan Pablo Mounier <juanpablomounier@gmail.com>
2026-02-17 13:35:36 -0300

    Implemented signal validation.

--- audio_inspector/validation.py
+++ audio_inspector/validation.py
@@ -5,10 +5,16 @@ Validation includes:
 1) Audio signal must be a non-empty array with finite values.
 2) Audio sample rate must be int or float and greater than 8000 Hz.
 """
-
+import numpy as np
 
 def validate_signal(audio_signal):
-    pass
+    if not isinstance(audio_signal,np.ndarray):
+        raise ValueError("Audio signal must be an array.")
+    if len(audio_signal) <= 0:
+        raise ValueError("The audio signal is empty.") 
+    if np.all(np.isfinite(audio_signal)) == False:
+        raise ValueError("Audio signal contains non-finite values.")
+    return True
 
 def validate_sample_rate(audio_sample_rate):
     if not isinstance(audio_sample_rate,(int,float)):
